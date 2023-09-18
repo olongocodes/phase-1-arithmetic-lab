69c6b27d1dd034a7d56128c82d59082407b40050
Kevin <kevinolongo9@gmail.com>
2023-09-19 01:26:27 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+
+const random = Math.ceil(Math.random() * Number.MAX_SAFE_INTEGER) + 1;
+
+const num3 = 19;
+const num4 = 5;
+const mod = num3 % num4;
+
+const numbers = [9, 3, 20, 17, 12];
+const max = Math.max(...numbers);
